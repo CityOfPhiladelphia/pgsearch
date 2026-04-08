1ca60236b89650fc9d2a8de8ddb87ae57721feb8
Darren McDowell <darren.mcdowell@Armins-Macbook-Pro.local>
2026-04-08 15:07:59 -0400

    chore: remove dead @phila/search-ingest package

--- packages/ingest/src/index.ts
+++
@@ -1,5 +0,0 @@
-// ABOUTME: Content parsing utilities for pgsearch document ingestion.
-// ABOUTME: Converts HTML and plain text into structured documents.
-
-export { parseHtml } from './html'
-export { parseText } from './text'
